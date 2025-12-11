5cd7f366bf1d92251a17645a13d19b66b26fd434
Abhay Sharma <abhay@techcrumb.com>
2025-12-11 19:05:40 +0530

    remove the cors

--- bknd/src/app.ts
+++ bknd/src/app.ts
@@ -13,10 +13,10 @@ app.use(express.json({
     }
 }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: true
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+//     credentials: true
+// }));
 app.use(helmet());
 app.use(morgan('dev'));
 
